d670e24b7923c4531cc4386905ae85ca9a639b4f
myasinalperen <myasinalperen@hotmail.com>
2024-10-12 20:30:09 +0300

    ilk commit

---
+++ FlightComputer/Core/Inc/IMU.h
@@ -0,0 +1,22 @@
+/*
+ * IMU.h
+ *
+ *  Created on: Oct 12, 2024
+ *      Author: m_yas
+ */
+
+#ifndef SRC_IMU_H_
+#define SRC_IMU_H_
+
+#include "stm32h7xx_hal.h"
+#include "main.h"
+
+class IMU {
+public:
+	IMU();
+	virtual ~IMU();
+	static void vIMUtask(void const* argument);
+
+};
+
+#endif /* SRC_IMU_H_ */
---
+++ FlightComputer/Core/Inc/NAV.h
@@ -0,0 +1,21 @@
+/*
+ * NAV.h
+ *
+ *  Created on: Oct 12, 2024
+ *      Author: m_yas
+ */
+
+#ifndef SRC_NAV_H_
+#define SRC_NAV_H_
+
+#include "stm32h7xx_hal.h"
+#include "main.h"
+
+class NAV {
+public:
+	NAV();
+	virtual ~NAV();
+	static void vNAVtask(void const* argument);
+};
+
+#endif /* SRC_NAV_H_ */
